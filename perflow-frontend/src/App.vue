13f4557ba86b8ab9921a4b67118a2086066f0e3e
eunseo76 <eunseolee19@gmail.com>
2024-12-04 21:30:52 +0900

    feat/common: #3 공용 버튼 - 크기 지정은 아직 안 함

--- perflow-frontend/src/App.vue
+++ perflow-frontend/src/App.vue
@@ -1,7 +1,20 @@
 <script setup>
+
+import ButtonBasic from "@/components/common/ButtonBasic.vue";
 </script>
 
 <template>
+  <br>
+  <p>*버튼 예시</p>
+  <ButtonBasic color="orange" label="기본 버튼1"/>
+  <ButtonBasic color="gray" label="기본 버튼2"/>
+  <br>
+  <ButtonBasic size="middle" label="기본 버튼3"/>
+  <br>
+  <ButtonBasic size="large" label="기본 버튼4"/>
+  <br>
+  <br>
+  <p>*표 예시</p>
 
 </template>
 
---
+++ perflow-frontend/src/components/common/ButtonBasic.vue
@@ -0,0 +1,78 @@
+<script>
+export const colors = ['orange', 'gray'];
+export const sizes = ['small', 'medium', 'large'];
+</script>
+
+<script setup>
+
+const emit = defineEmits(['click']);
+
+// const colors = ['orange', 'gray'];
+// const sizes = ['small', 'medium', 'large'];
+
+defineProps({
+  // 버튼 색상
+  color: {
+    type: String,
+    default: 'orange',
+    validator: (value) => colors.includes(value),  // 허용된 색상만 사용 가능
+  },
+  // 버튼 크기
+  size: {
+    type: String,
+    default: 'small',
+    validator: (value) => sizes.includes(value),
+  },
+  // 버튼 텍스트
+  label: {
+    type: String,
+    default: '버튼',
+  }
+})
+</script>
+
+<template>
+
+  <button
+      class ="btn"
+      :class="[`btn-${size}`, `btn-${color}`]"
+      @click="emit('click')"
+  >
+    <slot>{{ label }}</slot>
+
+  </button>
+
+</template>
+
+<style scoped>
+
+button > * {
+  font-weight: bold;
+}
+
+.btn-orange {
+  background-image: linear-gradient(to right, #f37321 0%, #fb0 100%);
+  color: white;
+  border: none;
+}
+
+.btn-gray {
+  background-color: #D9D9D9;
+  color: #3C4651;
+  border: none;
+}
+
+.btn-small {
+  padding: 5px 10px;
+  font-size: 12px;
+}
+
+.btn-medium {
+  padding: 10px 20px;
+  font-size: 16px;
+}
+.btn-large {
+  padding: 15px 30px;
+  font-size: 20px;
+}
+</style>
