7a2e2ba74168250dc8702ad99b90a6a9fde28c78
kimjm9911 <kimjm9911@naver.com>
2024-12-16 21:49:51 +0900

    feat: #11 드롭다운

---
+++ perflow-frontend/src/components/common/ButtonDropDown.vue
@@ -0,0 +1,138 @@
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+  options: {
+    type: Array,
+    default: () => [],
+  },
+  defaultOption: {
+    type: String,
+    default: "",
+  },
+  buttonStyle: {
+    type: String,
+    default: "orange", // 'orange' | 'gray'
+  },
+  width: {
+    type: String,
+    default: "150px", // 기본 너비
+  },
+  height: {
+    type: String,
+    default: "50px", // 기본 높이
+  },
+  fontSize: {
+    type: String,
+    default: "16px", // 글자 크기
+  },
+  imgSize: {
+    type: String,
+    default: "30px", // 이미지 크기
+  },
+});
+
+const emit = defineEmits(["select"]);
+
+const isMenuOpen = ref(false);
+const selectedOption = ref(props.defaultOption);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+const selectOption = (option) => {
+  selectedOption.value = option;
+  isMenuOpen.value = false;
+  emit("select", option);
+};
+</script>
+
+<template>
+  <div
+      class="dropdown-container"
+      :class="[`btn-${buttonStyle}`]"
+      :style="{ width: width, height: height, fontSize: fontSize }"
+      @click="toggleMenu"
+  >
+    <!-- 선택된 옵션 -->
+    <div class="selected-option">
+      <span>{{ selectedOption }}</span>
+      <img
+          v-if="isMenuOpen"
+          src="../../assets/image/arrow_up_2.png"
+          alt="up"
+          :style="{ width: imgSize, height: imgSize }"
+      />
+      <img
+          v-else
+          src="../../assets/image/arrow_down_2.png"
+          alt="down"
+          :style="{ width: imgSize, height: imgSize }"
+      />
+    </div>
+
+    <!-- 드롭다운 옵션 메뉴 -->
+    <ul
+        v-if="isMenuOpen"
+        class="options"
+        :style="{ fontSize: fontSize, width: width }"
+    >
+      <li
+          v-for="(option, index) in options"
+          :key="index"
+          @click.stop="selectOption(option.label)"
+      >
+        {{ option.label }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.dropdown-container {
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  position: relative;
+  border-radius: 5px;
+}
+
+.btn-gray {
+  background-color: white;
+  color: #3c4651;
+  border: 1px solid #afa9a9;
+}
+
+.selected-option {
+  padding: 8px;
+  display: flex;
+  align-items: center;
+}
+
+.options {
+  list-style: none;
+  padding: 0;
+  border: 1px solid #afa9a9;
+  position: absolute;
+  background-color: white;
+  color: #3c4651;
+  border-radius: 5px;
+  top: 100%; /* 버튼 바로 아래로 드롭다운 메뉴 표시 */
+  left: 0;   /* 왼쪽 정렬 */
+  z-index: 10; /* 다른 요소 위에 표시 */
+  margin-top: 4px;
+}
+
+.options li {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.options li:hover {
+  background-color: #f5f5f5;
+}
+
+</style>
