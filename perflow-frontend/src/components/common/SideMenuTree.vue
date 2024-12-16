73e5331429503c71ce2280f8d304ba33b2505799
JungUiJin <yealkki38@gmail.com>
2024-12-16 11:08:21 +0900

    feat: #10 메뉴 재귀 호출 기능 구현

--- perflow-frontend/src/components/common/SideMenuTree.vue
+++ perflow-frontend/src/components/common/SideMenuTree.vue
@@ -2,6 +2,8 @@
 
 import {onMounted} from "vue";
 import {useStore} from "@/store/store.js";
+import OrganizationSelectedEmp from "@/components/common/OrganizationSelectedEmp.vue";
+import SideMenuNode from "@/components/common/SideMenuNode.vue";
 
 const store = useStore();
 
@@ -12,9 +14,18 @@ onMounted(async () => {
 </script>
 
 <template>
-
+  <ul class="side_menu">
+    <!--    자식 컴포넌트 (재귀로 계속해서 호출) -->
+    <SideMenuNode
+        v-for="menu in store.$state.allMenu.filter(menu => !menu.parentId)"
+        :key=menu.menuId
+        :parent = menu
+    />
+  </ul>
 </template>
 
 <style scoped>
-
+.side_menu {
+  list-style: none;
+}
 </style>
