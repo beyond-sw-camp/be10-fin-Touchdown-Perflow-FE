7dc35625c7ac16ed14cba5b970eab21b363759bf
JungUiJin <yealkki38@gmail.com>
2024-12-16 11:08:21 +0900

    feat: #10 메뉴 재귀 호출 기능 구현

--- perflow-frontend/src/components/common/SideMenuNode.vue
+++ perflow-frontend/src/components/common/SideMenuNode.vue
@@ -1,11 +1,44 @@
 <script setup>
 
+import {useStore} from "@/store/store.js";
+import {ref} from "vue";
+
+const store = useStore();
+
+// 하위 메뉴 표시 상태 관리
+const isExpanded = ref(false);
+
+// 토글 함수
+const toggle = () => {
+  isExpanded.value = !isExpanded.value;
+};
+
+// Props로 메뉴 데이터 전달
+const props = defineProps({
+  parent : Object
+});
+
+const parentId = props.parent.parentId;
+
 </script>
 
 <template>
-
+  <li @click="toggle">
+    <!-- 현재 메뉴 이름 출력 -->
+    {{ props.parent.name }}
+    <!-- 하위 부서 렌더링 -->
+    <ul v-if="isExpanded" class="menu-child">
+      <SideMenuNode
+          v-for="child in store.$state.allMenu.filter(menu => menu.parentId === props.parent.id)"
+          :key="child.menuId"
+          :parent="child"
+      />
+    </ul>
+  </li>
 </template>
 
 <style scoped>
-
+.menu-child {
+  list-style: none;
+}
 </style>
