b38593620f187cf3537203a2188701104b68904e
JungUiJin <yealkki38@gmail.com>
2024-12-17 17:18:15 +0900

    feat: #24 로그아웃 타이머 구현

--- perflow-frontend/src/main.js
+++ perflow-frontend/src/main.js
@@ -3,11 +3,11 @@ import App from './App.vue'
 import './assets/global.css';
 import router from './router/router.js'
 import {createPinia} from "pinia";
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia();
-
+pinia.use(piniaPluginPersistedstate)
 const app = createApp(App);
-
 app.use(pinia);
 app.use(router);
 
