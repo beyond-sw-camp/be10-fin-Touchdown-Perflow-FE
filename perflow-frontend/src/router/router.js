e8af29dbe31cb065673aadf905552a3eca973320
enking <engur9902@gmail.com>
2024-12-18 19:25:52 +0900

    feat/attitude/#21 router 미사용 임포트문 삭제

--- perflow-frontend/src/router/router.js
+++ perflow-frontend/src/router/router.js
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EmpLogin from "@/views/hr/employee/EmpLogin.vue";
-import ApprovalHome from "@/views/ApprovalHome.vue";
-import newDoc from "@/views/newDoc.vue";
-import TemplateBasic from "@/views/TemplateBasic.vue";
 import WorkStatus from "@/views/Attitude/WorkStatus.vue";
 import WorkHours from "@/views/Attitude/WorkHours.vue";
 import Vacation from "@/views/Attitude/Vacation.vue";
