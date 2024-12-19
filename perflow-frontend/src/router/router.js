5bf3a24187c5ccef21328c9b966c3901cb1ad460
JungUiJin <yealkki38@gmail.com>
2024-12-19 11:15:09 +0900

    feat: #45 기본 구조 구현

--- perflow-frontend/src/router/router.js
+++ perflow-frontend/src/router/router.js
@@ -13,6 +13,7 @@ import WaitingDoc from "@/views/approval/WaitingDoc.vue";
 import EmpList from "@/views/hr/employee/EmpList.vue";
 import EmpListRegister from "@/views/hr/employee/EmpListRegister.vue";
 import PayrollList from "@/views/payment/PayrollList.vue";
+import EmpMyPage from "@/views/hr/employee/EmpMyPage.vue";
 
 
 const routes = [
@@ -20,6 +21,7 @@ const routes = [
 
     {path: '/', name: 'mainPage', component: EmpLogin},
     {path: '/login', name: 'Login', component: EmpLogin},
+    {path: '/hr/myPage', name: 'myPage', component: EmpMyPage},
     {path: '/hr/employees', name: 'EmpList', component: EmpList},
     {path: '/hr/employees/register/lists', name: 'EmpListRegister', component: EmpListRegister},
     {path: '/approval/home', name: 'approvalHome', component: ApprovalHome},
---
+++ perflow-frontend/src/views/hr/employee/EmpMyPage.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+import ButtonBack from "@/components/common/ButtonBack.vue";
+</script>
+
+<template>
+  <div id="my-page-container">
+    <div id="title-container">
+      <p>마이페이지</p>
+      <ButtonBack/>
+    </div>
+    <div id="info-header-container">
+    </div>
+    <div id="middle-line">
+
+    </div>
+    <div id="info-main-container">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#my-page-container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #3c4651 ;
+}
+#title-container, #info-header-container, #info-main-container {
+  width: 900px;
+}
+#title-container {
+  height: 150px;
+  display: flex;
+  justify-content: space-between;
+  padding-top: 50px;
+}
+#title-container > p {
+  font-weight: bold;
+  font-size: 35px;
+}
+</style>
